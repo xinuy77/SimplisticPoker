9d644eb27292e4dc0ec3e0767374a04003205c2c
xinuy77 <yuyamanaka@cmail.carleton.ca>
2018-09-28 13:02:44 -0400

    [testcase] added test strategy 1

---
+++ src/test/java/core/SimplisticPokerTest.java
@@ -0,0 +1,27 @@
+package core;
+
+import junit.framework.TestCase;
+
+public class SimplisticPokerTest extends TestCase {
+	 public void testStrategy1() {
+	        CardDeck        cardDeck        = new CardDeck();
+	        Card[]          straight        = cardDeck.getStraight();
+	        Card[]          royalFlush      = cardDeck.getRoyalFlush();
+	        Card[]          highCard        = cardDeck.getHighCard();
+	        Card[]          threeOfAKind    = cardDeck.getThreeOfAKind();
+	        Boolean         isAi            = true;
+	        Player          ai              = new Player(isAi);
+
+	        ai.setHand(straight);
+	        assertEquals(false, ai.wantsToExchange());
+
+	        ai.setHand(royalFlush);
+	        assertEquals(false, ai.wantsToExchange());
+
+	        ai.setHand(threeOfAKind);
+	        assertEquals(true, ai.wantsToExchange());
+
+	        ai.setHand(highCard);
+	        assertEquals(true, ai.wantsToExchange());
+	    }
+}
