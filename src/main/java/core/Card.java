f34aac1881863216106ec309dda7dcc73799c004
xinuy77 <yuyamanaka@cmail.carleton.ca>
2018-09-28 19:16:04 -0400

    [implementation] implementing Player class

--- src/main/java/core/Card.java
+++ src/main/java/core/Card.java
@@ -1,3 +1,4 @@
+package core;
 
 public class Card {
 	private String suit;
--- src/test/java/core/SimplisticPokerTest.java
+++ src/test/java/core/SimplisticPokerTest.java
@@ -3,25 +3,28 @@ package core;
 import junit.framework.TestCase;
 
 public class SimplisticPokerTest extends TestCase {
-	 public void testStrategy1() {
-	        CardDeck cardDeck     = new CardDeck();
-	        Card[]   straight     = cardDeck.getStraight();
-	        Card[]   royalFlush   = cardDeck.getRoyalFlush();
-	        Card[]   highCard     = cardDeck.getHighCard();
-	        Card[]   threeOfAKind = cardDeck.getThreeOfAKind();
-	        Boolean  isAi         = true;
-	        Player   ai           = new Player(isAi);
-
-	        ai.setHand(straight);
-	        assertEquals(false, ai.wantsToExchange());
-
-	        ai.setHand(royalFlush);
-	        assertEquals(false, ai.wantsToExchange());
-
-	        ai.setHand(threeOfAKind);
-	        assertEquals(true, ai.wantsToExchange());
-
-	        ai.setHand(highCard);
-	        assertEquals(true, ai.wantsToExchange());
-	    }
+	
+	private final String cardPath = "./src/main/resources/card.txt";
+	
+	public void testStrategy1() {
+		CardDeck cardDeck     = new CardDeck(cardPath);
+		Card[]   straight     = cardDeck.drawStraight();
+		Card[]   royalFlush   = cardDeck.drawRoyalFlush();
+		Card[]   highCard     = cardDeck.drawHighCard();
+		Card[]   threeOfAKind = cardDeck.drawThreeOfAKind();
+		Boolean  isAi         = true;
+		Player   ai           = new Player(null, isAi);
+		
+		ai.setHand(straight);
+		assertEquals(false, ai.wantsToExchange());
+		
+		ai.setHand(royalFlush);
+		assertEquals(false, ai.wantsToExchange());
+		
+		ai.setHand(threeOfAKind);
+		assertEquals(true, ai.wantsToExchange());
+		
+		ai.setHand(highCard);
+		assertEquals(true, ai.wantsToExchange());
+	}
 }
