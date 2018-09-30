0bd90b56e1c7fecdd52280b6dbd9d8d1220e240e
xinuy77 <yuyamanaka@cmail.carleton.ca>
2018-09-30 19:32:37 -0400

    [testcase] added test strategy 2

--- src/test/java/core/SimplisticPokerTest.java
+++ src/test/java/core/SimplisticPokerTest.java
@@ -27,4 +27,37 @@ public class SimplisticPokerTest extends TestCase {
 		ai.setHand(highCard);
 		assertEquals(true, ai.wantsToExchange());
 	}
+	
+	private void testOneCardAway(Player ai, Card[] hand, CardDeck cardDeck) {
+		ai.setHand(hand);
+		assertEquals(false, ai.wantsToExchange());
+		System.out.println(ai.getExchangeCard());
+		
+		for(int i = 0; i < hand.length; i++) {
+			Card[] tmp = hand;
+			tmp[i]     = cardDeck.drawCard();
+			ai.setHand(tmp);
+			assertEquals(true, ai.wantsToExchange());
+			System.out.println(ai.getExchangeCard());
+		}
+	}
+	
+	
+	public void testStrategy2() {
+	//	else if AIP is one card away from a RoyalFlush,aStraightFlush,aFullHouse,aFlush,oraStraight,it exchanges that card
+		CardDeck cardDeck      = new CardDeck(cardPath);
+		Card[]   royalFlush    = cardDeck.drawRoyalFlush();
+		Card[]   straightFlush = cardDeck.drawStraightFlush();
+		Card[]   fullHouse     = cardDeck.drawFullHouse();
+		Card[]   flush         = cardDeck.drawFlush();
+		Card[]   straight      = cardDeck.drawStraight();
+		Boolean  isAi          = true;   
+		Player   ai            = new Player(null, isAi);
+		
+		testOneCardAway(ai, royalFlush, cardDeck);
+		testOneCardAway(ai, straightFlush, cardDeck);
+		testOneCardAway(ai, fullHouse, cardDeck);
+		testOneCardAway(ai, flush, cardDeck);
+		testOneCardAway(ai, straight, cardDeck);
+	}
 }
