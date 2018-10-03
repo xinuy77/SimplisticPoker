272534408dd2ba9caa28cb3205f2ee6b7ad3a4a2
root <root@DESKTOP-F8NN4UM.localdomain>
2018-10-03 17:27:08 +0900

    [refactor] refactored several classes and added Hand class

--- src/main/java/core/CardDeck.java
+++ src/main/java/core/CardDeck.java
@@ -1,8 +1,6 @@
 package core;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
