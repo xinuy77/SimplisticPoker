baed13521878abe18d736a5b6ff7fb7616dae4cc
xinuy77 <yuyamanaka@cmail.carleton.ca>
2018-10-06 14:46:17 -0400

    [refactor] refactored test score 7

--- src/test/java/core/SimplisticPokerAcceptanceTest.java
+++ src/test/java/core/SimplisticPokerAcceptanceTest.java
@@ -16,41 +16,49 @@ public class SimplisticPokerAcceptanceTest extends TestCase {
 	}
 	
 	public void testScore1() {
-		String          inputPaths        = "./src/main/resources/bothRF.txt";
-		SimplisticPoker simplisticPoker   = new SimplisticPoker(inputPaths);
-		Player          ai                = simplisticPoker.getAI();
+		String          inputPaths      = "./src/main/resources/bothRF.txt";
+		SimplisticPoker simplisticPoker = new SimplisticPoker(inputPaths);
+		Player          ai              = simplisticPoker.getAI();
 		
 		assertEquals(true, simplisticPoker.aiWon());
 	}
 	
 	public void testScore2() {
-		String          inputPaths        = "./src/main/resources/bothSS.txt";
-		SimplisticPoker simplisticPoker   = new SimplisticPoker(inputPaths);
-		Player          ai                = simplisticPoker.getAI();
+		String          inputPaths      = "./src/main/resources/bothSS.txt";
+		SimplisticPoker simplisticPoker = new SimplisticPoker(inputPaths);
+		Player          ai              = simplisticPoker.getAI();
 		
 		assertEquals(true, simplisticPoker.aiWon());
 	}
 	
 	public void testScore3() {
-		String          inputPaths        = "./src/main/resources/bothFOK.txt";
-		SimplisticPoker simplisticPoker   = new SimplisticPoker(inputPaths);
-		Player          ai                = simplisticPoker.getAI();
+		String          inputPaths      = "./src/main/resources/bothFOK.txt";
+		SimplisticPoker simplisticPoker = new SimplisticPoker(inputPaths);
+		Player          ai              = simplisticPoker.getAI();
 		
 		assertEquals(true, simplisticPoker.aiWon());
 	}
 	
 	public void testScore4() {
-		String          inputPaths        = "./src/main/resources/bothFHandTOK.txt";
-		SimplisticPoker simplisticPoker   = new SimplisticPoker(inputPaths);
-		Player          ai                = simplisticPoker.getAI();
+		String          inputPaths      = "./src/main/resources/bothFHandTOK.txt";
+		SimplisticPoker simplisticPoker = new SimplisticPoker(inputPaths);
+		Player          ai              = simplisticPoker.getAI();
 		
 		assertEquals(true, simplisticPoker.aiWon());
 	}
 	
 	public void testScore6() {
-		String          inputPaths        = "./src/main/resources/bothS.txt";
-		SimplisticPoker simplisticPoker   = new SimplisticPoker(inputPaths);
-		Player          ai                = simplisticPoker.getAI();
+		String          inputPaths      = "./src/main/resources/bothS.txt";
+		SimplisticPoker simplisticPoker = new SimplisticPoker(inputPaths);
+		Player          ai              = simplisticPoker.getAI();
+		
+		assertEquals(true, simplisticPoker.aiWon());
+	}
+	
+	public void testScore7() {
+		String          inputPaths      = "./src/main/resources/bothTP.txt";
+		SimplisticPoker simplisticPoker = new SimplisticPoker(inputPaths);
+		Player          ai              = simplisticPoker.getAI();
 		
 		assertEquals(true, simplisticPoker.aiWon());
 	}
